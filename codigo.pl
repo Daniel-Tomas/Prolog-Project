323877ad8590ba5bca14608b44a658a50584c5b9
Daniel Tomas <danieltomas-sanchez-8@hotmail.com>
2021-05-10 12:42:53 +0200

    Added stage B (apparently working) and type checking in paretensis to pass 1 test

--- codigo.pl
+++ codigo.pl
@@ -1,10 +1,19 @@
 :- module(_,_,[classic,assertions,regtypes]).
+
 alumno_prode('Cabero','Blanco','Aaron','180440').
+
+%% :- prop alumno_prode/4 #"@includedef{alumno_prode/4}".
+%% alumno_prode('Tomas', 'Sanchez', 'Daniel', 'a180428').
+
+%% :- doc(title, "Second Proyect - Pure logic programming").
+
+%% :- doc(author, "Daniel Tomas Sanchez, 180428").
 
 
 comprimir(Inicial, Comprimida):-limpia_memo, compresion_recursiva(Inicial, Comprimida).
 limpia_memo.
-compresion_recursiva(Inicial, Comprimida):-repeticion(Inicial, Comprimida).
+compresion_recursiva(Inicial, Comprimida):-
+    compresion(Inicial, Comprimida).
 compresion_recursiva(Inicial, Inicial).
 
 %PRELIMINARES
@@ -13,8 +22,12 @@ compresion_recursiva(Inicial, Inicial).
 partir(Todo, Parte1, Parte2):-append(Parte1,Parte2,Todo), length(Parte1,N1), N1>0, length(Parte2,N2), N2>0. 
 
 %Parentesis/3
-parentesis(Parte,Num,ParteNum):-length(Parte,N), N>=2, append(['('],Parte,L1), append(L1,[')',Num],ParteNum).
-parentesis(Parte,Num,ParteNum):-length(Parte,N), N<2, append(Parte,[Num],ParteNum).
+parentesis(Parte,Num,ParteNum):- integer(Num), length(Parte,N), N>=2, append(['('],Parte,L1), append(L1,[')',Num],ParteNum).
+parentesis(Parte,Num,ParteNum):- integer(Num), length(Parte,N), N<2, append(Parte,[Num],ParteNum).
+
+%% parentesis(Parte,Num,ParteNum):-  append(L1,[')',Num],ParteNum), append(['('],Parte,L1),integer(Num),  length(Parte,N), N>=2.
+%% parentesis(Parte,Num,ParteNum):-  append(Parte,[Num],ParteNum), integer(Num),  length(Parte,N), N<2.
+
 
 %Se_repite/4
 %Auxiliar de Se_repite
@@ -31,7 +44,28 @@ se_repite(Cs,Parte,N0,Num):-length(Cs, Ls), length(Parte, La),T is Ls//La, copia
 repeticion(Inicial, Comprimida):-partir(Inicial, Parte1, _), se_repite(Inicial,Parte1,0,R), compresion_recursiva(Parte1, Comp),
     parentesis(Comp,R,Comprimida).
 
-compresion(X,Y).
+
+% FASE B
+division(Inicial, Comprimida) :-
+    partir(Inicial, Parte1, Parte2),
+    compresion_recursiva(Parte1, Comprimida1),
+    compresion_recursiva(Parte2, Comprimida2),
+    append(Comprimida1, Comprimida2, Comprimida),
+    Inicial \= Comprimida.
+
+
+%% compresion(Inicial, Comprimida) :-
+%%     division(Inicial, Comprimida).
+
+compresion(Inicial, Comprimida) :-
+    repeticion(Inicial, Comprimida).
+
+compresion(Inicial, Comprimida) :-
+    division(Inicial, Comprimida).
+
+
+
+%% compresion(X,Y).
 mejor_compresion(X,Y).
 mejor_compresion_memo(X,Y).
 memo(X,Y).
